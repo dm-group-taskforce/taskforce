016abfb99429db57ed157a4b59f8285c2d2fc25f
Jeremy Mark <jeremymark@Christinas-MacBook-Pro.local>
2019-10-22 10:41:54 -0500

    added task reducer

--- src/Components/NewTask/NewTask.js
+++ src/Components/NewTask/NewTask.js
@@ -1,15 +1,66 @@
-
 import React from "react";
+import {connect} from 'react-redux';
+import {getUserTask, addTask, editTask, deleteTask} from '../../redux/reducers/taskReducer';
 
+class NewTask extends React.Component {
 
-function NewTask() {
-    return (
-        <main>
-            <div>
+    constructor() {
+        super();
+        this.state = {}            
+    }
 
-                <p>Add new tasks</p>
-            </div>
-        </main>
-    )
+    componentDidMount() {
+        this.props.getUserTask(this.props.match.params.task_id);
+    }
+
+    handleAddTask() {
+        const {addTask} = this.props;
+        addTask()
+    }
+
+    handleDeleteTask(task_id) {
+        const {deleteTask} = this.props;
+        deleteTask(task_id); 
+    }
+
+    render() {
+        const taskDisplay = this.props.tasks.map((tasks, index) => {
+            return (
+                <div className="task-container">
+                    <div key={index} className='task-item'>                     
+                        <p>{tasks}</p>
+                        <button 
+                        className="delete-task-btn"
+                        type="button"
+                        onClick={() => this.handleDeleteTask(this.props.match.params.task_id)}
+                        >Delete</button>                                  
+                    </div>               
+                </div>
+            )
+        })
+
+        return (
+            <main>
+                <div>
+                    <p>Add new tasks</p>
+                </div>
+                {taskDisplay}
+            </main>
+        )
+    }
 }
-export default NewTask;
+
+const mapStateToProps = reduxState => {
+    return {
+        tasks: reduxState.cardReducer.tasks
+    }
+}
+
+export default connect(mapStateToProps, 
+    {
+        getUserTask,
+        addTask,
+        editTask, 
+        deleteTask
+    }
+)(NewTask);
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,13 @@ import promise from 'redux-promise-middleware';
 import userReducer from './reducers/userReducer'
 import characterReducer from './reducers/characterReducer'
 import rankReducer from './reducers/rankReducer'
-
+import taskReducer from './reducers/taskReducer'
 
 const rootReducer = combineReducers({
     userReducer,
     characterReducer,
-    rankReducer
+    rankReducer,
+    taskReducer
 })
 
 export default createStore(rootReducer, applyMiddleware(promise));
