87bf00c46ac576b88cd35f3bc70deb6e3a021772
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-10-23 15:54:01 -0500

    chart updates when user finishes a task

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import userReducer from './reducers/userReducer'
 import characterReducer from './reducers/characterReducer'
 import rankReducer from './reducers/rankReducer'
 import taskReducer from './reducers/taskReducer'
+import chartReducer from './reducers/chartReducer'
 
 const rootReducer = combineReducers({
     userReducer,
     characterReducer,
     rankReducer,
-    taskReducer
+    taskReducer,
+    chartReducer
 })
 
 export default createStore(rootReducer, applyMiddleware(promise));
