235385fb835fc1fb59dfc6b7ca2a764f1a377eee
rgjohnson322 <rgjohnson322@gmail.com>
2019-10-31 11:20:44 -0500

    carousel functionality and design

--- src/Components/Carousel/carouselinfo.js
+++ src/Components/Carousel/carouselinfo.js
@@ -1,17 +1,23 @@
 import XXX from '../../Assets/xxx.png'
+import "./carouselinfo.scss"
 
 const info = [
     {
-        content: '1',
-        img: XXX
+        content: 'Are you ready to become your best self?',
+        img: XXX,
+        style: 'card1',
+        color: 'rgb(186,186,186)',
+        borderColor: '#13e6d8'
     },
     {
         content: '2',
-        img: ''
+        img: '',
+        style: 'card2'
     },
     {
         content: '3',
-        img: ''
+        img: '',
+        style: 'card3'
     }
 ]
 
