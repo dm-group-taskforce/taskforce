f92e5e7351509c79023027d2f8c7710a3ce5200d
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-10-31 16:59:24 -0500

    the user now needs to level up to access second game

--- src/Components/Games/Games.js
+++ src/Components/Games/Games.js
@@ -1,50 +1,84 @@
-
-import React from "react";
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import "./Games.scss";
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux'
 
+export class Games extends Component {
 
-function Games() {
-
-    let butts = {
-        backgroundColor: '#13e6d8',
-        color: 'black',
-        width: '8vw',
-        fontSize: '1.5vw'
+    constructor(){
+        super();
+        this.state = {
+            style: ''
+        }
     }
-    return (
-        <main className="gwholething">
-            <div className="angry">
-            <div className="gheader">
-                <h1>MAKE SURE YOU ARE IN FULL SCREEN MODE BEFORE PRESSING PLAY</h1>
-            </div>
-            <div className=" gbutts">
-                <div className="game1">
-                    <div className="gbutt1">
-                        <Link to='/games/1'><Button
-                            style={butts}
-                            variant="contained"
-                            color="primary"
-                        >PLAY LEVEL 1</Button></Link>
-                    </div>
 
+    componentDidMount(){
+        let check = this.props.rank.toLowerCase();
+        if (check === 'pvt' || check === 'pfc' || check === 'lcpl' || check === 'cpl' || check === 'sgt'){
+            this.setState({style: 'greyOut'})
+        }
+        else{
+            this.setState({style: ''})
+        }
+    }
+
+    render() {
+        let butts = {
+            backgroundColor: '#13e6d8',
+            color: 'black',
+            width: '8vw',
+            fontSize: '1.5vw'
+        }
+        return (
+            <main className="gwholething">
+                <div className="angry">
+                <div className="gheader">
+                    <h1>MAKE SURE YOU ARE IN FULL SCREEN MODE BEFORE PRESSING PLAY</h1>
                 </div>
-                <div className="game2">
-                    <div className="gbutt2">
-                        <Link to='/games/1'><Button
-                            style={butts}
-                            variant="contained"
-                            color="primary"
-                        >PLAY LEVEL 2</Button></Link>
+                <div className="gbutts">
+                    <div className='game1'>
+                        <div className="gbutt1">
+                            <Link to='/games/1'><Button
+                                style={butts}
+                                variant="contained"
+                                color="primary"
+                            >PLAY LEVEL 1</Button></Link>
+                        </div>
+    
                     </div>
-
+                    <div className={`game2 ${this.state.style}`}>
+                        <div className="gbutt2">
+                            <>
+                            {this.state.style !== 'greyOut'? 
+                              <Link to='/games/1'><Button
+                              style={butts}
+                              variant="contained"
+                              color="primary"
+                              >PLAY LEVEL 2</Button></Link>:
+                              null
+                        
+                            }
+                            </>
+                            
+                        </div>
+    
+                    </div>
+    
+    
                 </div>
-
-
-            </div>
-            </div>
-        </main>
-    )
+                </div>
+            </main>
+        )
+    }
 }
-export default Games;
+
+const mapStateToProps = (reduxState) => ({
+    rank: reduxState.rankReducer.abbreviation
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Games)
